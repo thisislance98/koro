de18f2d771e45b10979f83c1dbb734a0fb4849ca
thisislance98 <thisislance98@gmail.com>
2025-02-02 00:21:46 +0000

    test

---
+++ modules/zuplo.runtime.ts
@@ -0,0 +1,18 @@
+import {
+  RuntimeExtensions,
+  StripeMonetizationPlugin,
+  environment,
+} from "@zuplo/runtime";
+
+
+export function runtimeInit(runtime: RuntimeExtensions) {
+  // Create the Stripe Plugin
+  const stripe = new StripeMonetizationPlugin({
+    webhooks: {
+      signingSecret: environment.STRIPE_WEBHOOK_SIGNING_SECRET,
+    },
+    stripeSecretKey: environment.STRIPE_SECRET_KEY,
+  });
+  // Register the plugin
+  runtime.addPlugin(stripe);
+}
